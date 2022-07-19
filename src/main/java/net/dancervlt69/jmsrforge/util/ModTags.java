a6e7fdd6fd62bb8f61ebd15455056604fe5941d3
DancerVlt69 <dancervlt69@googlemail.com>
2022-07-19 10:44:09 +0200

    removed: duplicates and unused files

---
+++ src/main/java/net/dancervlt69/jmsrforge/util/ModTags.java
@@ -0,0 +1,40 @@
+package net.dancervlt69.jmsrforge.util;
+
+import net.dancervlt69.jmsrforge.JmsrForgeMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+
+public class ModTags {
+
+    public static class Blocks {
+
+//       private static Tags.IOptionalNamedTag<Block> createTag(String name) {
+//            return BlockTags.createOptional(new ResourceLocation(JmsrForgeMod.MODID, name));
+        }
+
+//        private static Tags.IOptionalNamedTag<Block> createForgeTag(String name) {
+//            return BlockTags.createOptional(new ResourceLocation( "forge", name));
+//        }
+
+    }
+
+//    public static class Items {
+
+//        public static final Tags.IOptionalNamedTag<Item> BLADING_TOOLS = createForgeTag("blading_tools");
+
+//        private static Tags.IOptionalNamedTag<Item> createTag(String name) {
+//            return ItemTags.createOptional(new ResourceLocation(JmsrForgeMod.MODID, name));
+//        }
+
+//        private static Tags.IOptionalNamedTag<Item> createForgeTag(String name) {
+//            return ItemTags.createOptional(new ResourceLocation( "forge", name));
+//        }
+
+
+//    }
+
+//}
